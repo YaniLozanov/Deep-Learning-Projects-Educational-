c718435c593795f7af46cd477743bba50e777370
YaniLozanov <kronos1997@abv.bg>
2019-08-13 14:39:47 +0300

    Upload Eventures Project

---
+++ Eventures/Eventures.Data/EventuresDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Eventures.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Eventures.Data
+{
+    public class EventuresDbContext : IdentityDbContext<EventuresUser, IdentityRole, string>
+    {
+
+        public EventuresDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Eventures/Eventures.Domain/EventuresUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace Eventures.Domain
+{
+    public class EventuresUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UCN { get; set; }
+
+
+    }
+}
