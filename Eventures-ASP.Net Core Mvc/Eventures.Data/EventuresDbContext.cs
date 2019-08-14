08f829cb87e1cdf20767e36aee2080ee01f698ca
YaniLozanov <kronos1997@abv.bg>
2019-08-14 13:18:32 +0300

    Eventures project ready!

--- Eventures-ASP.Net Core Mvc/Eventures.Data/EventuresDbContext.cs
+++ Eventures-ASP.Net Core Mvc/Eventures.Data/EventuresDbContext.cs
@@ -2,16 +2,13 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
 
 namespace Eventures.Data
 {
     public class EventuresDbContext : IdentityDbContext<EventuresUser, IdentityRole, string>
     {
+        public DbSet<Event> Events { get; set; }
 
-        public EventuresDbContext(DbContextOptions options) : base(options)
-        {
-
-        }
+        public EventuresDbContext(DbContextOptions<EventuresDbContext> options) : base(options) { }
     }
 }
--- Eventures-ASP.Net Core Mvc/Eventures.Domain/EventuresUser.cs
+++ Eventures-ASP.Net Core Mvc/Eventures.Domain/EventuresUser.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
 
 namespace Eventures.Domain
 {
     public class EventuresUser : IdentityUser
     {
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
         public string UCN { get; set; }
-
-
     }
 }
