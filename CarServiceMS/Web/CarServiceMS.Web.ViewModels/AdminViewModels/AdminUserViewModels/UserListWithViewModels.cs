148dd982a3cc27dd39ddb497104a301b85e75b47
Yani Lozanov <32339328+YaniLozanov@users.noreply.github.com>
2021-11-10 08:46:45 +0100

    Delete CarServiceMS directory

--- CarServiceMS/Web/CarServiceMS.Web.ViewModels/AdminViewModels/AdminUserViewModels/UserListWithViewModels.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace CarServiceMS.Web.ViewModels.AdminViewModels.AdminUserViewModels
-{
-    public class UserListWithViewModels
-    {
-        public IEnumerable<UserListingViewModel> Users { get; set; }
-    }
-}
